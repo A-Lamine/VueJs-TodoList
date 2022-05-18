e703aab48c6c31b485d7d2d5168625ab1d1651e6
Lamine ADEL <lamine.adel@expaceo.com>
2022-05-18 17:51:53 +0200

    AddStore

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex';
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+const store = createStore({
+    state() {
+
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    }
+});
+
+app.use(router).use(store).mount('#app')
